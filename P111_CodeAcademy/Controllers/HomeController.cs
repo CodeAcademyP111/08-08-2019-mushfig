97e706b0a2e4c89470334e2fa337042deb11d7b9
Mushfig0101 <mushfigma@code.edu.az>
2019-08-18 19:43:55 +0400

    Front Final Core

---
+++ P111_CodeAcademy/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace P111_CodeAcademy.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region
+        //public IActionResult Index()
+        //{
+        //    return Json(new
+        //    {
+
+        //        id = 1,
+        //        name = "cavid",
+        //        surname = "bashirov"
+        //    });
+
+        // return File("~/img/cards_img/1.jpg","image/jpg");
+
+        // }
+        #endregion
+
+        public  IActionResult Error()
+        {
+            return Content("Siz gonderdiyiniz url duzgun deyil");
+        }
+    }
+}
